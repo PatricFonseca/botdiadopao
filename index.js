1d1403d392b5341c8590bf07a96eba5a57c59da1
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2023-05-08 21:58:33 -0300

    update bot with gif

--- index.js
+++ index.js
@@ -9,34 +9,35 @@ const { DISCORD_TOKEN, CHANNEL_NAME } = process.env;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const PEOPLE = [
-  "Richard",
-  "Patric",
-  "Pamela",  
-  "Emmanuel",
-  "Rolyson",
-  "Eliane",
-  "Charles",
+  {nome: "Pamela", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105200543103717507/Video_sem_titulo_Feito_com_o_Clipchamp_1.gif'}  ,
+  {nome: "Emmanuel", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105202380104024204/Video_sem_titulo_Feito_com_o_Clipchamp_4.gif'},
+  {nome: "Rolyson", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105215285851586630/Video_sem_titulo_Feito_com_o_Clipchamp.gif'},
+  {nome: "Eliane", gif: 'https://media.tenor.com/AkILqGsdIFoAAAAC/pokemon-ash.gif'},
+  {nome: "Charles", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105215228033126510/Video_sem_titulo_Feito_com_o_Clipchamp_5.gif'},
+  {nome: "Richard", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105202380976439376/Video_sem_titulo_Feito_com_o_Clipchamp_2.gif'},
+  { nome: "Patric", gif: 'https://media.discordapp.net/attachments/1103657441389981740/1105202380611530893/Video_sem_titulo_Feito_com_o_Clipchamp_3.gif' }
 ];
 
-const WEEK = ["Sexta", "Segunda", "Terça", "Quarta", "Quinta"];
+const WEEK = ["Terça", "Quarta", "Quinta", "Sexta", "Segunda"];
 const HOLIDAYS = [];
 
-let peopleList = PEOPLE;
+let peopleList = PEOPLE.slice();
 let weekList = WEEK;
 let lastPeopleRemoved = [];
 let channel = null;
 let bread_leo = false
+let response = ""
 
 function getPeopleDay() {
   if (peopleList.length === 0) {
-    peopleList = PEOPLE;
+    peopleList = PEOPLE.slice();
   }
 
   const person = peopleList.shift();
   console.log(person);
 
   if (weekList.length === 0) {
-    weekList = WEEK;
+    weekList = WEEK.slice();
   }
 
   const day = weekList.shift();
@@ -48,9 +49,15 @@ function getPeopleDay() {
     bread_quantity += 1 
   }
 
-  lastPeopleRemoved.push(person + " " + day + ":" + bread_quantity + " pães " );
+  response = person.nome + " " + day + ":" + bread_quantity + " pães " + " " + person.gif 
 
-  channel.send(lastPeopleRemoved[lastPeopleRemoved.length - 1]);
+  // lastPeopleRemoved.push(person.nome + " " + day + ":" + bread_quantity + " pães " );
+
+  // channel.send(lastPeopleRemoved[lastPeopleRemoved.length - 1]);
+  channel.send(response)
+
+  //Removo independente se foi alterado ou nÃo
+  remove_leo_bread();
   // return timePeople + weekday
 }
 
@@ -73,7 +80,9 @@ function getNextPersonOnQueue() {
 
   let quantity = bread_leo ? PEOPLE.length + 1 : PEOPLE.length  
 
-  channel.send(person + " " + day + ": " + quantity + " pães" )
+  console.log(person.nome + " " + day + ": " + quantity + " pães")
+
+  return person.nome + " " + day + ": " + quantity + " pães" + " " + person.gif
 
 }
 
@@ -108,8 +117,19 @@ client.on("ready", async () => {
 
   await loadHolidays();
 
-  channel = client.channels.cache.find((ch) => ch.name === CHANNEL_NAME);
-  channel.send("hello world!");
+  // Itera sobre todos os servidores em que o bot está presente
+  client.guilds.cache.forEach(guild => {
+    // Envia a mensagem para o canal geral de cada servidor
+    channel = guild.channels.cache.find(channel => channel.name === CHANNEL_NAME);
+    
+    if (channel && channel.type === 0){
+      // channel.send('Teste!');
+    }
+  });
+  
+
+  // channel = client.channels.cache.find((ch) => ch.name === CHANNEL_NAME);
+  // channel.send("hello world!");
 });
 
 function getLista() {
@@ -137,7 +157,7 @@ client.on("interactionCreate", async (interaction) => {
   if (interaction.commandName === "ping") {
     await interaction.reply("Pong!");
   } else if (interaction.commandName === "amanha") {
-    getNextPersonOnQueue()
+    await interaction.reply(getNextPersonOnQueue())
     // const pessoaDoDia = getPessoaDoDia();
     // await interaction.reply(`Hoje é dia de ${pessoaDoDia} trazer o pão`);
   } else if (interaction.commandName === "lista") {
--- index_old.js
+++ index_old.js
@@ -1,39 +1,135 @@
-import {Client} from 'discord.js'
-import { config } from 'dotenv'
+import { Client, GatewayIntentBits } from "discord.js";
+import { config } from "dotenv";
+import cron from "node-cron";
+import { api } from "./api.js";
 config();
 
+const { DISCORD_TOKEN, CHANNEL_NAME } = process.env;
 
-// const Discord = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const client = new Client({ intents: [] });
-const PREFIX = '!';
-
-const lista = [
-  { dia: 'Segunda-feira', pessoa: 'Fulano' },
-  { dia: 'Terça-feira', pessoa: 'Ciclano' },
-  { dia: 'Quarta-feira', pessoa: 'Beltrano' },
-  { dia: 'Quinta-feira', pessoa: 'Deltrano' },
-  { dia: 'Sexta-feira', pessoa: 'Estrano' },
+const PEOPLE = [
+  "Patric",
+  "Pamela",  
+  "Emmanuel",
+  "Rolyson",
+  "Eliane",
+  "Charles",
+  "Richard",
 ];
 
-function getPessoaDoDia() {
-  const hoje = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const diaDaSemana = hoje.toLocaleDateString('pt-BR', options)
-  // const diaDaSemana = hoje.toLocaleDateString('pt-BR', { weekday: 'segunda-feira' });
+const WEEK = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
+const HOLIDAYS = [];
 
-  for (let i = 0; i < lista.length; i++) {
-    const item = lista[i];
-    if (item.dia === diaDaSemana) {
-      return item.pessoa;
-    }
+let peopleList = PEOPLE.slice();
+let weekList = WEEK;
+let lastPeopleRemoved = [];
+let channel = null;
+let bread_leo = false
+
+function getPeopleDay() {
+  if (peopleList.length === 0) {
+    peopleList = PEOPLE;
   }
 
-  return 'Não há pão hoje';
+  const person = peopleList.shift();
+  console.log(person);
+
+  if (weekList.length === 0) {
+    weekList = WEEK;
+  }
+
+  const day = weekList.shift();
+  console.log(day);
+
+  let bread_quantity = PEOPLE.length
+
+  if (bread_leo) {
+    bread_quantity += 1 
+  }
+
+  lastPeopleRemoved.push(person + " " + day + ":" + bread_quantity + " pães " );
+
+  channel.send(lastPeopleRemoved[lastPeopleRemoved.length - 1]);
+
+  //Removo independente se foi alterado ou nÃo
+  remove_leo_bread();
+  // return timePeople + weekday
 }
 
+function getNextPersonOnQueue() {
+  let person = null
+  let day = null
+
+  if (peopleList.length > 0) { 
+    person = peopleList[0]
+  } else {
+    person = PEOPLE[0]
+  }
+
+  if (weekList.length > 0) { 
+    day = weekList[0]
+  }
+  else {
+    day = WEEK[0]
+  }
+
+  let quantity = bread_leo ? PEOPLE.length + 1 : PEOPLE.length  
+
+  console.log(person + " " + day + ": " + quantity + " pães")
+
+  return person + " " + day + ": " + quantity + " pães" 
+
+}
+
+function removeElement() {
+  // Obtenha a hora atual
+  const now = new Date();
+
+  // Formatando a data atual para o formato yyyy-mm-dd
+  const formatedDate = `${now.getFullYear()}-${(now.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}`;
+
+  // está entre segunda e sexta
+  if (now.getDay() >= 1 && now.getDay() <= 5) {
+    // se não é um feriado
+    if (!HOLIDAYS.includes(formatedDate)) {
+      getPeopleDay();
+    }
+  }
+  // }
+}
+
+async function loadHolidays() {
+  const res = await api.get("2023/BR");
+  res.data.map((data) => {
+    HOLIDAYS.push(data.date);
+  });
+}
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  await loadHolidays();
+
+  // Itera sobre todos os servidores em que o bot está presente
+  client.guilds.cache.forEach(guild => {
+    // Envia a mensagem para o canal geral de cada servidor
+    channel = guild.channels.cache.find(channel => channel.name === CHANNEL_NAME);
+    // if (channel && channel.type === 'text') {
+    if (channel && channel.type === 0){
+      channel.send('Teste!');
+    }
+  });
+  
+
+  // channel = client.channels.cache.find((ch) => ch.name === CHANNEL_NAME);
+  // channel.send("hello world!");
+});
+
 function getLista() {
-  let listaTexto = 'Lista de pessoas para o pão da semana:\n\n';
+  let listaTexto = "Lista de pessoas para o pão da semana:\n\n";
 
   for (let i = 0; i < lista.length; i++) {
     const item = lista[i];
@@ -43,28 +139,34 @@ function getLista() {
   return listaTexto;
 }
 
-client.on('message', (message) => {
-  console.log(message.channel);
-  if (message.author.bot) return;
+function add_leo_bread() {
+  bread_leo = true
+}
 
-  if (message.content.startsWith(PREFIX)) {
-    const [command, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
+function remove_leo_bread() { 
+  bread_leo = false
+}
 
-    if (command === 'paododia') {
-      const pessoaDoDia = getPessoaDoDia();
-      message.channel.send(`Hoje é dia de ${pessoaDoDia} trazer o pão`);
-    } else if (command === 'listapao') {
-      const listaTexto = getLista();
-      message.channel.send(listaTexto);
-    }
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (interaction.commandName === "amanha") {
+    await interaction.reply(getNextPersonOnQueue())
+    // const pessoaDoDia = getPessoaDoDia();
+    // await interaction.reply(`Hoje é dia de ${pessoaDoDia} trazer o pão`);
+  } else if (interaction.commandName === "lista") {
+    await interaction.reply(`lista: ${getLista()}`);
+  } else if (interaction.commandName === "leo_quer_pao") {
+    add_leo_bread()
+  } else if (interaction.commandName === "leo_nao_quer_pao") {
+    remove_leo_bread()
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(DISCORD_TOKEN);
 
-
-// const message = { content: '!paododia', channel: 'teste', author: { bot: false } };
-// client.emit('message', message);
+cron.schedule("0 9 * * *", () => {
+  removeElement();
+});
