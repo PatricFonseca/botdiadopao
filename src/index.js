63cd6e580233e01b542d7fb41d3e5775f773a07d
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2024-02-15 13:08:03 -0300

    chore: separate list of people in another file

--- src/index.js
+++ src/index.js
@@ -1,43 +1,13 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
 import cron from "node-cron";
-import { api } from "../lib/api.js";
+import { api } from "./lib/api.js";
+import { PEOPLE } from "./lib/constants/people-list.js";
 config();
 
 const { DISCORD_TOKEN, CHANNEL_NAME } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-const PEOPLE = [
-  {
-    nome: "Pamela",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105200543103717507/Video_sem_titulo_Feito_com_o_Clipchamp_1.gif",
-  },
-  {
-    nome: "Emmanuel",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380104024204/Video_sem_titulo_Feito_com_o_Clipchamp_4.gif",
-  },
-  {
-    nome: "Rolyson",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105215285851586630/Video_sem_titulo_Feito_com_o_Clipchamp.gif",
-  },
-  {
-    nome: "Eliane",
-    gif: "https://media.tenor.com/AkILqGsdIFoAAAAC/pokemon-ash.gif",
-  },
-  {
-    nome: "Charles",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105215228033126510/Video_sem_titulo_Feito_com_o_Clipchamp_5.gif",
-  },
-  {
-    nome: "Richard",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380976439376/Video_sem_titulo_Feito_com_o_Clipchamp_2.gif",
-  },
-  {
-    nome: "Patric",
-    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380611530893/Video_sem_titulo_Feito_com_o_Clipchamp_3.gif",
-  },
-];
 
 const WEEK = ["Terça", "Quarta", "Quinta", "Sexta", "Segunda"];
 const HOLIDAYS = [];
---
+++ src/lib/constants/people-list.js
@@ -0,0 +1,30 @@
+export const PEOPLE = [
+  {
+    nome: "Rolyson",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105215285851586630/Video_sem_titulo_Feito_com_o_Clipchamp.gif",
+  },
+  {
+    nome: "Eliane",
+    gif: "https://media.tenor.com/AkILqGsdIFoAAAAC/pokemon-ash.gif",
+  },
+  {
+    nome: "Charles",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105215228033126510/Video_sem_titulo_Feito_com_o_Clipchamp_5.gif",
+  },
+  {
+    nome: "Richard",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380976439376/Video_sem_titulo_Feito_com_o_Clipchamp_2.gif",
+  },
+  {
+    nome: "Patric",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380611530893/Video_sem_titulo_Feito_com_o_Clipchamp_3.gif",
+  },
+  {
+    nome: "Pamela",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105200543103717507/Video_sem_titulo_Feito_com_o_Clipchamp_1.gif",
+  },
+  {
+    nome: "Emmanuel",
+    gif: "https://media.discordapp.net/attachments/1103657441389981740/1105202380104024204/Video_sem_titulo_Feito_com_o_Clipchamp_4.gif",
+  },
+];
